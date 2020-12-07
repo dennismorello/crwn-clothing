70106702534eb4e2b865ecd0d2d5187e6f229904
dennismorello <dennis.morello@contentwise.com>
2020-12-07 19:23:33 +0100

    added type="button" to Sign in with Google button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -64,7 +64,11 @@ class SignIn extends Component {
 
           <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton isGoogleSignIn onClick={signInWithGoogle}>
+            <CustomButton
+              type="button"
+              isGoogleSignIn
+              onClick={signInWithGoogle}
+            >
               Sign in with Google
             </CustomButton>
           </div>
